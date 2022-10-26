899f5a99281f24d20f53f3a932352e016f665a90
gregdott <dott.greg@gmail.com>
2022-10-26 14:17:09 +0100

    Update MergeSort.java
    Finished first draught of iterative version.
    Still need to do comments and potentially some optimisation. Doing a slightly weird thing in iterative version, so need to see if there is a more optimal way of achieving the same effect.

--- Algos/MergeSort.java
+++ Algos/MergeSort.java
@@ -29,9 +29,83 @@ import Utils.Pr;
 public class MergeSort {
     public static void main(String args[]) {
         List<Integer> unsorted = new ArrayList<Integer>(Arrays.asList(1, 9, 3, 7, 33, 42, 2, 3, 4));
-        List<Integer> sorted = mergeSortTopDown(unsorted);
         Pr.x("Unsorted: " + unsorted.toString());
-        Pr.x("Sorted: " + sorted.toString());
+       
+
+        //-----------------------------------------------------------------------
+        // Iterative:
+        Pr.x("----------------------------------------------------");
+        Pr.x("Merge Sort (Iterative - Bottom Up):");
+        List<Integer> sortedIterative = mergeSortBottomUp(unsorted);
+        Pr.x("SORTED: " + sortedIterative.toString());
+        Pr.x("----------------------------------------------------");
+        //-----------------------------------------------------------------------
+
+        //-----------------------------------------------------------------------
+        // Recursive:
+        Pr.x("----------------------------------------------------");
+        Pr.x("Merge Sort (Recursive - Top Down):");
+        List<Integer> sortedRecursive = mergeSortTopDown(unsorted);
+        Pr.x("Sorted: " + sortedRecursive.toString());
+        Pr.x("----------------------------------------------------");
+        //-----------------------------------------------------------------------
+    }
+
+    public static List<Integer> mergeSortBottomUp(List<Integer> unsorted) {
+        List<Integer> sorting = new ArrayList<Integer>(unsorted);
+        
+
+        /*
+         * To do this bottom up (iteratively), my first thought is to do it like this:
+         * Loop through list and compare successive sets of neighbours (0 & 1, 2 & 3 etc.) and reorganise pairs in order.
+         * Then on next loop compare sets of sets so [0, 1] with [2, 3] and reorganise. Then [0, 1, 2, 3] with [4, 5, 6, 7]
+         * We do this until the last iteration where the size of our first set added to the size of our second set is equal to the length of the list.
+         * 
+         * 
+         */
+
+        int bracketSize = 2;
+
+        while (bracketSize*2 < unsorted.size()) {
+            List<Integer> sortStep = new ArrayList<Integer>();
+            for (int i = 0; i < unsorted.size()/bracketSize; i = i + 2) {
+                int minEnd = (i*bracketSize) + bracketSize*2;
+
+                if (minEnd > unsorted.size()) {
+                    minEnd = unsorted.size();
+                }
+
+                List<Integer> arr1 = new ArrayList<Integer>(sorting.subList(i*bracketSize, (i*bracketSize) + bracketSize));
+                List<Integer> arr2 = new ArrayList<Integer>(sorting.subList((i*bracketSize) + bracketSize, minEnd));
+                List<Integer> merged = merge(arr1, arr2);
+
+                sortStep.addAll(merged);
+            }
+            
+            // Well, this works now. Not sure if it is unnecessarily complicated though? It almost certainly does extra work compared to the recursive version.
+            // Nevertheless, when doing comments I need to put a good explanation of what is being done here.
+            if (sorting.size()%bracketSize > 0) {
+                int startNeglected = (sorting.size()/bracketSize)*bracketSize;
+                int endNeglected = startNeglected + (sorting.size()%bracketSize);
+
+                sortStep.addAll(sorting.subList(startNeglected, endNeglected));
+
+                List<Integer> arr1 = new ArrayList<Integer>(sortStep.subList(startNeglected - bracketSize, startNeglected));
+                List<Integer> arr2 = new ArrayList<Integer>(sortStep.subList(startNeglected, endNeglected));
+                List<Integer> merged = merge(arr1, arr2);
+
+                // remove last bracket plus neglected from sortStep and add merged back
+                sortStep = sortStep.subList(0, startNeglected - bracketSize);
+                sortStep.addAll(merged);
+            }
+
+            // not sure if we have to copy here as sortStep gets reinstantiated each iteration. Must test to see what happens if we don't copy and just use a reference.
+            sorting = new ArrayList<Integer>(sortStep);
+            bracketSize = bracketSize*2;
+        }
+        
+        // sorting ArrayList is now sorted (or at least it should be!)
+        return sorting;
     }
 
     public static List<Integer> mergeSortTopDown(List<Integer> unsorted) {
