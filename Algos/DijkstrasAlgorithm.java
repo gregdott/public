0c57a358394b018be0e203511da45df83821b61e
gregdott <dott.greg@gmail.com>
2022-10-31 15:23:50 +0000

    Update DijkstrasAlgorithm.java
    Commenting and neatening

--- Algos/DijkstrasAlgorithm.java
+++ Algos/DijkstrasAlgorithm.java
@@ -33,7 +33,6 @@ public class DijkstrasAlgorithm {
     public static void main(String args[]) {
         int[] dist, prev;
         List<Integer> unvisited;
-        
         int[][] weightedEdges = {{0, 1, 10}, {0, 4, 3}, {1, 2, 2}, {1, 4, 4}, {2, 3, 9}, {3, 2, 7}, {4, 1, 1}, {4, 2, 8}, {4, 3, 2}};
 
         int numVertices = 5;
@@ -42,82 +41,113 @@ public class DijkstrasAlgorithm {
         dist = new int[numVertices];
         prev = new int[numVertices];
         unvisited = new ArrayList<Integer>();
-
-        initDataStructures(dist, prev, unvisited, startVertex);
-        calculateShortestPath(dist, prev, unvisited, wg);
-        //printShortestPaths(startVertex, dist, prev);
-
-        for (int i = 0; i < dist.length; i++) {
-            if (i == startVertex) continue;
-            Pr.x("Path from " + startVertex + " to " + i);
-            Pr.x("Distance: " + dist[i]);
-
-            List<Integer> path = reconstructPath(startVertex, i, prev);
-            for (int j = path.size() - 1; j >= 0; j--) {
-                Pr.x("(" + path.get(j) + ")");
-            }
-        }
         
+        calculateShortestPaths(dist, prev, unvisited, wg, startVertex);
+        printShortestPaths(startVertex, dist, prev);
     }
 
-    private static void printShortestPaths(int startVertex, int[] dist, int[] prev) {
-        for (int i = 0; i < dist.length; i++) {
-            for (int j = 0; j < dist.length; j++) {
-                List<Integer> path = reconstructPath(i, j, prev);
-                Pr.x(path.toString());
-            }
-        }
-        
-    }
-
-    private static List<Integer> reconstructPath(int startVertex, int dest, int[] prev) {
-        int currentVertex = dest;
-        List<Integer> path = new ArrayList<Integer>();        
-        path.add(currentVertex);
-        
-        while(currentVertex != startVertex) {
-            currentVertex = prev[currentVertex];
-            path.add(currentVertex);
-        }
-
-        return path;
-    }
-
-    private static void calculateShortestPath(int[] dist, int[] prev, List<Integer> unvisited, WeightedGraph wg) {
+    /**
+     * calculateShortestPath - calculates the shortest path from the startVertex to all other vertices in the graph
+     * using Dijkstra's Algorithm
+     * 
+     * @param dist distances from startVertex (while calculating this stores current min distance from start to i)
+     * @param prev tree structure storing paths to startVertex
+     * @param unvisited list of unvisited vertices
+     * @param wg weighted graph object
+     * @param startVertex the vertex we begin the search from
+     */
+    private static void calculateShortestPaths(int[] dist, int[] prev, List<Integer> unvisited, WeightedGraph wg, int startVertex) {
+        initDataStructures(dist, prev, unvisited, startVertex); // initialise the data structures we are using
         List<List<Integer>> adjList = wg.getAdjList();
         while(!unvisited.isEmpty()) { // while we have vertices in our unvisited list
-            int minDist = Integer.MAX_VALUE;
+            int minDist = Integer.MAX_VALUE; // initialise to largest possible value
             int currentVertex = -1;
             
+            // Go through list of currently unvisited vertices and find the one that is closest to startVertex
             for (Integer uv: unvisited) { // uv = unvisited vertex
                 if (dist[uv] < minDist) {
                     minDist = dist[uv];
                     currentVertex = uv;
                 }
             }
-            unvisited.remove(Integer.valueOf(currentVertex));
+            unvisited.remove(Integer.valueOf(currentVertex)); // remove currentVertex from our list of unvisited vertices
 
             // visit all the neighbours of currentVertex
             List<Integer> neighbours = adjList.get(currentVertex);            
             for (Integer neighbour: neighbours) {
                 int newDist = dist[currentVertex] + wg.getEdgeWeight(currentVertex, neighbour);
-                if (newDist < dist[neighbour]) {
+                if (newDist < dist[neighbour]) { // if the distance to the neighbour on the current path is less than what we have before, update the distance and the path
                     dist[neighbour] = newDist;
                     prev[neighbour] = currentVertex;
                 }
             }
-
         }
     }
 
+    /**
+     * initDataStructures - initialise the data structures necessary for calculating the shortest paths
+     * 
+     * @param dist distances from startVertex (while calculating this stores current min distance from start to i)
+     * @param prev tree structure storing paths to startVertex
+     * @param unvisited list of unvisited vertices
+     * @param startVertex the vertex we begin the search from
+     */
     private static void initDataStructures(int[] dist, int[] prev, List<Integer> unvisited, int startVertex) {
         for (int i = 0; i < dist.length; i++) {
-            dist[i] = Integer.MAX_VALUE;
+            dist[i] = Integer.MAX_VALUE; // initialise to largest possible value
             prev[i] = -1;
             unvisited.add(i);
         }
         dist[startVertex] = 0;
     }
+
+    /**
+     * printShortestPaths - after calculating the shortest paths from the source vertex to all other nodes,
+     * print the distance and the reconstructed paths
+     * 
+     * @param startVertex the vertex our search began from
+     * @param dist distances from startVertex
+     * @param prev tree structure storing paths to startVertex
+     */
+    private static void printShortestPaths(int startVertex, int[] dist, int[] prev) {
+        for (int i = 0; i < dist.length; i++) {
+            if (i == startVertex) continue;
+            Pr.x("Path from " + startVertex + " to " + i);
+            Pr.x("Distance: " + dist[i]);
+
+            List<Integer> path = reconstructPath(startVertex, i, prev);
+            int[] pathReversed = new int[path.size()];
+            int count = 0;
+            for (int j = path.size() - 1; j >= 0; j--) { // this just reverses the path so we can see it in the order that the nodes were visited
+                pathReversed[count] = path.get(j);
+                count++;
+            }
+            Pr.x(Arrays.toString(pathReversed));
+        }
+    }
+
+    /**
+     * reconstructPath - reconstruct the shortest path from startVertex to dest vertex
+     * 
+     * @param startVertex the vertex our search began from
+     * @param dest destination vertex
+     * @param prev tree structure storing paths to startVertex
+     * @return
+     */
+    private static List<Integer> reconstructPath(int startVertex, int dest, int[] prev) {
+        int currentVertex = dest;
+        List<Integer> path = new ArrayList<Integer>();        
+        path.add(currentVertex);
+        
+        while(currentVertex != startVertex) { // while we have not found our way back to the start vertex
+            currentVertex = prev[currentVertex]; // traverse up the tree to the previous vertex from the current one
+            path.add(currentVertex);
+        }
+
+        return path;
+    }
+
+    
 }
 
 
